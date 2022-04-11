9592d01c57be4357d565d96ee914b157fb1def6b
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-04-10 19:48:38 -0500

    clase 7

---
+++ intro/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem'; 
+import { CreateTodoButton} from './CreateTodoButton';
+// import './App.css';
+
+
+const todos = [
+  { text: 'Cortar cebolla', completed:false },
+  { text: 'Tomar el curso de intro a React', completed:false },
+  { text: 'Llorar con la llorona', completed:false }
+]
+
+function App() { // componente  con la sintaxis de react
+  return (     // en react nos pide usar solo una etiqueta, si ponemos mas no nos permite//
+    <React.Fragment> 
+    {/* una etiqueta por componente */}
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.text} text={todo.text}/>
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export default App;
+// {} con las llaves , llamamos variables en js,
