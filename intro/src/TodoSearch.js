aa59b8b6961e9aa57a7d4c1b1c3942ff8a4e13b7
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-04-10 22:13:52 -0500

    clase9

--- intro/src/TodoSearch.js
+++ intro/src/TodoSearch.js
@@ -1,18 +1,24 @@
 import React from "react";
 import './TodoSearch.css';
 
+
 function TodoSearch(){
+    const [searchValue, setSearchValue] = React.useState(''); // forma de agregar estado en componentes al crearlas como funciones
+   
     const onSearchValueChange = (event) => {
-        console.log(event.target.value)
+        console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return(
+    return[
         <input 
         className="TodoSearch" 
         placeholder="Cebolla" 
+        value={searchValue}
             onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
 export {TodoSearch};
