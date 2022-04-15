8f0419ed5ab903e76ccc08bc8f1f497b1109eaea
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-04-14 21:39:19 -0500

    clase 13

--- intro/src/App/index.js
+++ intro/src/App/index.js
@@ -1,66 +1,74 @@
 import React from 'react';
 import { AppUi } from './AppUi';
 
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: true },
+//   { text: 'LALALALAA', completed: false },
+// ];
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar el curso de intro a React', completed: true },
-  { text: 'Llorar con la llorona', completed:false }
-]
+function App() {
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+  let parsedTodos;
 
-function App() { // componente  con la sintaxis de react
-  const [todos, setTodos] = React.useState(defaultTodos);
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  
+  const [todos, setTodos] = React.useState(parsedTodos);
   const [searchValue, setSearchValue] = React.useState('');
 
-  const completedTodos = todos.filter(todo => !!todo.completed ).length;
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
-  
+
   let searchedTodos = [];
 
   if (!searchValue.length >= 1) {
     searchedTodos = todos;
   } else {
-    searchedTodos = todos.filter( todo => {
-      const todoText = todo.text.toLowercase();
-      const searchText = searchValue.toLocaleLowerCase();
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText);
     });
   }
+
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
 
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
-    // todos[todoIndex].completed = true;
-    
-    // todos[todoIndex] = {
-    //   text: todos[todoIndex].text,
-    //   completed: true,
-    // };
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex(todo => todo.text === text);
     const newTodos = [...todos];
-    newTodos.splice(todoIndex,1);
-    setTodos(newTodos);
+    newTodos.splice(todoIndex, 1);
+    saveTodos(newTodos);
   };
-
-
-
-  return (     // en react nos pide usar solo una etiqueta, si ponemos mas no nos permite//
-  <AppUi 
-    totalTodos={totalTodos}
-    completedTodos={completedTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-    completeTodo ={completeTodo}
-    deleteTodo={deleteTodo}
-  />
+  
+  return (
+    <AppUi
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
 export default App;
-// {} con las llaves , llamamos variables en js,
