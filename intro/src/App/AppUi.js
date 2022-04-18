49e29115181181de03411000bc7f6c355ca04727
Luis Angel Perez Melendez <angelmelen88@gmail.com>
2022-04-18 18:58:15 -0500

    clase 16

--- intro/src/App/AppUi.js
+++ intro/src/App/AppUi.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { TodoContext } from '../TodoContext';
 import { TodoCounter } from '../TodoCounter';
 import { TodoSearch } from '../TodoSearch';
 import { TodoList } from '../TodoList';
@@ -6,46 +7,41 @@ import { TodoItem } from '../TodoItem';
 import { CreateTodoButton} from '../CreateTodoButton';
 // import './App.css';
 
-function AppUi({
-    loading,
-    error,
-    totalTodos,
-    completedTodos,
-    searchValue,
-    setSearchValue,
-    searchedTodos,
-    completeTodo,
-    deleteTodo,
-}) {
+function AppUi() {
     return (
         <React.Fragment> 
         {/* una etiqueta por componente */}
-          <TodoCounter 
-            total={totalTodos}
-            completed={completedTodos}
-          />
-          <TodoSearch 
-            searchValue={searchValue}
-            setSearchValue={setSearchValue}
-    
-          />
-          <TodoList>
-          {/* estado de carga */}
-            {loading && <p>Estamos cargando</p>}
-            {error && <p>me da anciedad...</p>}
-            {(!loading && !searchedTodos.length) && <p>menos ansiedad</p>} 
+          <TodoCounter/>
+          <TodoSearch/>
+          
+          <TodoContext.Consumer>
+            { ({
+              error,
+              loading,
+              completeTodo,
+              deleteTodo,
+              searchedTodos 
+              }) => (
+              <TodoList>
+                {/* estado de carga */}
+                {loading && <p>Estamos cargando</p>}
+                {error && <p>me da anciedad...</p>}
+                {(!loading && !searchedTodos.length) && <p>menos ansiedad</p>} 
 
 
-            {searchedTodos.map(todo => (
-              <TodoItem 
-              key={todo.text} 
-              text={todo.text}
-              completed={todo.completed}
-              onComplete={() => completeTodo(todo.text)}
-              ondelete={() => deleteTodo(todo.text)}
-              />
-            ))}
-          </TodoList>
+                {searchedTodos.map(todo => (
+                  <TodoItem 
+                  key={todo.text} 
+                  text={todo.text}
+                  completed={todo.completed}
+                  onComplete={() => completeTodo(todo.text)}
+                  ondelete={() => deleteTodo(todo.text)}
+                  />
+                ))}
+              </TodoList>
+                )}
+          </TodoContext.Consumer>
+          
           <CreateTodoButton />
         </React.Fragment>
     );
